49225ad654238bf5c87b7624d3c5f54d99467588
Dimitris Damilos <d.k.damilos@gmail.com>
2018-09-22 17:00:56 +0100

    :art: Add Success message

--- index.php
+++ index.php
@@ -8,22 +8,28 @@
         <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
-        <div class="container">
+        <main class="container">
             <h1 class="text-center">Image Collection File Renamer</h1>
             <form method="post" action="">
                 <div class="form-group">
-                <label for="file-group-label">Absolute Local Directory</label>
-                <input type="text" class="form-control" name="local-directory" placeholder="i.e. D:\images">
-            </div>
+                    <label for="file-group-label">Absolute Local Directory</label>
+                    <input type="text" class="form-control" name="local-directory" placeholder="i.e. D:\images">
+                </div>
 
-            <div class="form-group">
-                <label for="file-group-label">Label (optional)</label>
-                <input type="text" class="form-control" name="file-group-label" placeholder="Label">
-            </div>
+                <div class="form-group">
+                    <label for="file-group-label">Label (optional)</label>
+                    <input type="text" class="form-control" name="file-group-label" placeholder="Label">
+                </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+
+                <div class="alert alert-success<?php if ((empty($_REQUEST['r'])) || $_REQUEST['r'] !== 'success') { echo ' hidden'; } ?>">
+                    <strong>Success!</strong> All files have been renamed.
+                </div>
             </form>
-        </div>
+        </main>
         <span><?php include_once "app/get_values.php"; include_once "app/renamer.php"; ?></span>
     </body>
 </html>
